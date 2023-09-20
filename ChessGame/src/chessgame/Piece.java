e56411644c4b123228f8c88ac12d9e4b9e3f4fd3
Giancarlos Rodriguez <91751980+grodr08@users.noreply.github.com>
2023-09-20 22:11:07 +0100

    Constructed Piece Object class and created method move and Kill

--- ChessGame/src/chessgame/Piece.java
+++ ChessGame/src/chessgame/Piece.java
@@ -1,4 +1,32 @@
 package chessgame;
 
+import java.util.LinkedList;
+
 public class Piece {
+    int xp;
+    int yp;
+    boolean isWhite;
+    LinkedList<Piece> ps;
+
+    public Piece(int xp, int yp, boolean isWhite, LinkedList<Piece> ps) {
+        this.xp = xp;
+        this.yp = yp;
+        this.isWhite = isWhite;
+        this.ps = ps;
+        ps.add(this);
+    }
+
+    public void move(int xp, int yp) {
+        for (Piece p: ps) {
+            if (p.xp == xp && p.yp == yp) {
+                p.kill();
+            }
+        }
+        this.xp = xp;
+        this.yp = yp;
+    }
+
+    public void kill() {
+        ps.remove(this);
+    }
 }
