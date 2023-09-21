7fffcbdc28b59040653f9fe81ddb47e85a7a4521
Giancarlos Rodriguez <91751980+grodr08@users.noreply.github.com>
2023-09-21 21:45:23 +0100

    createed an instance of every piece on the board

--- ChessGame/src/chessgame/ChessGame.java
+++ ChessGame/src/chessgame/ChessGame.java
@@ -15,6 +15,7 @@ public class ChessGame {
         BufferedImage all = ImageIO.read(new File("C:\\Users\\gianc\\Git_Hub_Repositories\\ChessGame\\ChessGame\\src\\chessgame\\chess.png"));
         Image imgs[] = new Image[12];
 
+        // Cuts the images of the pieces from the imported image
         int ind = 0;
         for (int y = 0; y < 400; y += 200) {
             for (int x = 0; x < 1200; x += 200) {
@@ -23,9 +24,43 @@ public class ChessGame {
             }
         }
 
-        Piece wking = new Piece(5, 2, true, "king", ps);
-        Piece bpawn = new Piece(5, 3, false, "pawn", ps);
-        Piece wrook = new Piece(5, 5, true, "rook", ps);
+        // Black Pieces on Board
+        Piece bRook1 = new Piece(0, 0, false, "rook", ps);
+        Piece bKnight1 = new Piece(1, 0, false, "knight", ps);
+        Piece bBishop1 = new Piece(2, 0, false, "bishop", ps);
+        Piece bQueen = new Piece(3, 0, false, "queen", ps);
+        Piece bKing = new Piece(4, 0, false, "king", ps);
+        Piece bBishop2 = new Piece(5, 0, false, "bishop", ps);
+        Piece bKnight2 = new Piece(6, 0, false, "knight", ps);
+        Piece bRook2 = new Piece(7, 0, false, "rook", ps);
+
+        Piece bPawn1 = new Piece(1, 1, false, "pawn", ps);
+        Piece bPawn2 = new Piece(2, 1, false, "pawn", ps);
+        Piece bPawn3 = new Piece(3, 1, false, "pawn", ps);
+        Piece bPawn4 = new Piece(4, 1, false, "pawn", ps);
+        Piece bPawn5 = new Piece(5, 1, false, "pawn", ps);
+        Piece bPawn6 = new Piece(6, 1, false, "pawn", ps);
+        Piece bPawn7 = new Piece(7, 1, false, "pawn", ps);
+        Piece bPawn8 = new Piece(0, 1, false, "pawn", ps);
+
+        // White Pieces on Board
+        Piece wRook1 = new Piece(0, 7, true, "rook", ps);
+        Piece wKnight1 = new Piece(1, 7, true, "knight", ps);
+        Piece wBishop1 = new Piece(2, 7, true, "bishop", ps);
+        Piece wQueen = new Piece(3, 7, true, "queen", ps);
+        Piece wKing = new Piece(4, 7, true, "king", ps);
+        Piece wBishop2 = new Piece(5, 7, true, "bishop", ps);
+        Piece wKnight2 = new Piece(6, 7, true, "knight", ps);
+        Piece wRook2 = new Piece(7, 7, true, "rook", ps);
+
+        Piece wPawn1 = new Piece(1, 6, true, "pawn", ps);
+        Piece wPawn2 = new Piece(2, 6, true, "pawn", ps);
+        Piece wPawn3 = new Piece(3, 6, true, "pawn", ps);
+        Piece wPawn4 = new Piece(4, 6, true, "pawn", ps);
+        Piece wPawn5 = new Piece(5, 6, true, "pawn", ps);
+        Piece wPawn6 = new Piece(6, 6, true, "pawn", ps);
+        Piece wPawn7 = new Piece(7, 6, true, "pawn", ps);
+        Piece wPawn8 = new Piece(0, 6, true, "pawn", ps);
 
         JFrame frame = new JFrame();
         frame.setBounds(10, 10, 528, 551);
@@ -34,8 +69,12 @@ public class ChessGame {
             @Override
             public void paint(Graphics g) {
                 boolean white = true;
+
+                // Loop that Draws the Board
                 for (int i = 0; i < 8; i++) {
                     for (int j = 0; j < 8; j++) {
+
+                        // Fills in the squares with colors
                         if (white) {
                             g.setColor(new Color(235, 235, 208));
                         }
@@ -49,6 +88,7 @@ public class ChessGame {
                     }
                     white = !white;
                 }
+
                 for (Piece p: ps) {
                     int ind = 0;
 
