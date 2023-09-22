f2ee830b561645de9a25559202bad084938f4218
Giancarlos Rodriguez <91751980+grodr08@users.noreply.github.com>
2023-09-22 03:41:48 +0100

    created new MouseMotionListener and new MouseListener class

--- ChessGame/src/chessgame/ChessGame.java
+++ ChessGame/src/chessgame/ChessGame.java
@@ -3,6 +3,10 @@ package chessgame;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -118,6 +122,46 @@ public class ChessGame {
             }
         };
         frame.add(panel);
+        
+        frame.addMouseMotionListener(new MouseMotionListener() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+            }
+        });
+
+        frame.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
         frame.setDefaultCloseOperation(3);
         frame.setVisible(true);
     }
